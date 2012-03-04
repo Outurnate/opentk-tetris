2a8a16adeeb88089e64d51a8da970274c52fbfbf
Joseph Dillon <joe@w00t42.net>
2012-03-04 13:26:03 -0500

    remove random.org reference

--- lib/download.sh
+++ lib/download.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-rm -rf "EJ Random.Org Consumer" "opentk"
-wget http://www.random.org/clients/http/archive/2008-06-26/ej_random_org_consumer.zip
-unzip ej_random_org_consumer.zip
-rm -rf ej_random_org_consumer.zip "Compiled Test App"
-cp "./Compiled Class Library/EJ Random.Org Consumer.dll" "../bin/EJ Random.Org Consumer.dll"
-mkdir "EJ Random.Org Consumer"
-mv "Compiled Class Library" "EJ Random.Org Consumer"
-mv "Source Code" "EJ Random.Org Consumer"
+rm -rf "opentk"
 wget --trust-server-name http://sourceforge.net/projects/opentk/files/opentk/opentk-1.0/2010-10-06/opentk-2010-10-06.zip/download
 unzip opentk-2010-10-06.zip
 rm opentk-2010-10-06.zip
