9500e1c813b8959077578e9a668496168ddd8cfb
Joseph Dillon <joe@w00t42.net>
2012-03-16 11:00:49 -0400

    added headers...

--- FieldLogic.cs
+++ FieldLogic.cs
@@ -1,3 +1,26 @@
+//|-----------------------------------------------------------------------|\\
+//| FieldLogic.cs                                                         |\\
+//| Calculates tetramino movement and updates renderer                    |\\
+//|                                                                       |\\
+//| Joseph Dillon                                                         |\\
+//|-----------------------------------------------------------------------|\\
+//| opentk-tetris, a tetris game                                          |\\
+//| Copyright (C) 2012  Joseph Dillon                                     |\\
+//|                                                                       |\\
+//| This program is free software: you can redistribute it and/or modify  |\\
+//| it under the terms of the GNU General Public License as published by  |\\
+//| the Free Software Foundation, either version 3 of the License, or     |\\
+//| (at your option) any later version.                                   |\\
+//|                                                                       |\\
+//| This program is distributed in the hope that it will be useful,       |\\
+//| but WITHOUT ANY WARRANTY; without even the implied warranty of        |\\
+//| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         |\\
+//| GNU General Public License for more details.                          |\\
+//|                                                                       |\\
+//| You should have received a copy of the GNU General Public License     |\\
+//| along with this program.  If not, see <http://www.gnu.org/licenses/>. |\\
+//|-----------------------------------------------------------------------|\\
+
 using GameFramework;
 using System;
 using System.Collections.Generic;
