1289e3e12f55dcb672a253208cc35a80108259b9
Joseph Dillon <joe@w00t42.net>
2012-04-10 16:31:48 -0400

    updated build files

--- lib/download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget --trust-server-name http://sourceforge.net/projects/opentk/files/opentk/opentk-1.0/2010-10-06/opentk-2010-10-06.zip/download
-unzip opentk-2010-10-06.zip
-rm opentk-2010-10-06.zip
