f3104ea416b8eb66353a6be7749325b738d6185f
Joseph Dillon <joe@w00t42.net>
2012-04-14 11:50:51 -0400

    implemented input controls

--- CoordinatedInputManager.cs
+++ CoordinatedInputManager.cs
@@ -37,12 +37,46 @@ using OpenTK.Input;
 
 namespace Tetris
 {
-  class CoordinatedInputManager : GameComponent
+  class CoordinatedInputManager
   {
     public enum PlayerNumber { PlayerOne = 1, PlayerTwo = 2, PlayerThree = 3, PlayerFour = 4 }
 
-    struct KeyMap   { public Key  rotate, softDrop, left, right, hardDrop, hold; }
-    struct KeyState { public bool rotate, softDrop, left, right, hardDrop, hold; }
+    class KeyMap   { public Key    rotate, softDrop, left, right, hardDrop, hold; }
+    class KeyState { public bool   rotate, softDrop, left, right, hardDrop, hold; }
+    class KeyDelay { public double rotate, softDrop, left, right, hardDrop, hold; }
+    public class RepeatedKeyState
+    {
+      public bool Rotate
+      {
+	get;
+	internal set;
+      }
+      public bool SoftDrop
+      {
+	get;
+	internal set;
+      }
+      public bool Left
+      {
+	get;
+	internal set;
+      }
+      public bool Right
+      {
+	get;
+	internal set;
+      }
+      public bool HardDrop
+      {
+	get;
+	internal set;
+      }
+      public bool Hold
+      {
+	get;
+	internal set;
+      }
+    }
     Dictionary<PlayerNumber, KeyMap> maps = new Dictionary<PlayerNumber, KeyMap>()
     {
       {
@@ -132,15 +166,125 @@ namespace Tetris
 	new KeyState()
       }
     };
+    Dictionary<PlayerNumber, KeyDelay> delayState = new Dictionary<PlayerNumber, KeyDelay>()
+    {
+      {
+	PlayerNumber.PlayerOne,
+	new KeyDelay()
+      },
+      {
+	PlayerNumber.PlayerTwo,
+	new KeyDelay()
+      },
+      {
+	PlayerNumber.PlayerThree,
+	new KeyDelay()
+      },
+      {
+	PlayerNumber.PlayerFour,
+	new KeyDelay()
+      }
+    };
+    public Dictionary<PlayerNumber, RepeatedKeyState> KeyStates
+    {
+      get;
+      private set;
+    }
+    double delayTime = 1;
+    InterleavedFieldManager.NumPlayers players;
+    GameWindow window;
 
-    public CoordinatedInputManager(GameWindow window) : base (window) { }
+    public CoordinatedInputManager(GameWindow window, InterleavedFieldManager.NumPlayers players)
+    {
+      this.players = players;
+      this.window = window;
+      this.KeyStates = new Dictionary<PlayerNumber, RepeatedKeyState>()
+      {
+	{
+	  PlayerNumber.PlayerOne,
+	  new RepeatedKeyState()
+	},
+	{
+	  PlayerNumber.PlayerTwo,
+	  new RepeatedKeyState()
+	},
+	{
+	  PlayerNumber.PlayerThree,
+	  new RepeatedKeyState()
+	},
+	{
+	  PlayerNumber.PlayerFour,
+	  new RepeatedKeyState()
+	}
+      };
+    }
 
-    protected override void DoUnLoad() { }
+    public void Begin(FrameEventArgs e)
+    {
+      for (int i = 1; i < (int)players + 1; i++)
+      {
+	currentState[(PlayerNumber)i].rotate   = this.window.Keyboard[maps[(PlayerNumber)i].rotate];
+	currentState[(PlayerNumber)i].softDrop = this.window.Keyboard[maps[(PlayerNumber)i].softDrop];
+	currentState[(PlayerNumber)i].left     = this.window.Keyboard[maps[(PlayerNumber)i].left];
+	currentState[(PlayerNumber)i].right    = this.window.Keyboard[maps[(PlayerNumber)i].right];
+	currentState[(PlayerNumber)i].hardDrop = this.window.Keyboard[maps[(PlayerNumber)i].hardDrop];
+	currentState[(PlayerNumber)i].hold     = this.window.Keyboard[maps[(PlayerNumber)i].hold];
+	if (KeyStates[(PlayerNumber)i].Rotate   = (currentState[(PlayerNumber)i].rotate   && !previousState[(PlayerNumber)i].rotate))   delayState[(PlayerNumber)i].rotate   += delayTime;
+	if (KeyStates[(PlayerNumber)i].SoftDrop = (currentState[(PlayerNumber)i].softDrop && !previousState[(PlayerNumber)i].softDrop)) delayState[(PlayerNumber)i].softDrop += delayTime;
+	if (KeyStates[(PlayerNumber)i].Left     = (currentState[(PlayerNumber)i].left     && !previousState[(PlayerNumber)i].left))     delayState[(PlayerNumber)i].left     += delayTime;
+	if (KeyStates[(PlayerNumber)i].Right    = (currentState[(PlayerNumber)i].right    && !previousState[(PlayerNumber)i].right))    delayState[(PlayerNumber)i].right    += delayTime;
+	if (KeyStates[(PlayerNumber)i].HardDrop = (currentState[(PlayerNumber)i].hardDrop && !previousState[(PlayerNumber)i].hardDrop)) delayState[(PlayerNumber)i].hardDrop += delayTime;
+	if (KeyStates[(PlayerNumber)i].Hold     = (currentState[(PlayerNumber)i].hold     && !previousState[(PlayerNumber)i].hold))     delayState[(PlayerNumber)i].hold     += delayTime;
+	if (currentState[(PlayerNumber)i].rotate)   delayState[(PlayerNumber)i].rotate   -= e.Time;
+	if (currentState[(PlayerNumber)i].softDrop) delayState[(PlayerNumber)i].softDrop -= e.Time;
+	if (currentState[(PlayerNumber)i].left)     delayState[(PlayerNumber)i].left     -= e.Time;
+	if (currentState[(PlayerNumber)i].right)    delayState[(PlayerNumber)i].right    -= e.Time;
+	if (currentState[(PlayerNumber)i].hardDrop) delayState[(PlayerNumber)i].hardDrop -= e.Time;
+	if (currentState[(PlayerNumber)i].hold)     delayState[(PlayerNumber)i].hold     -= e.Time;
+	if (delayState[(PlayerNumber)i].rotate < 0)
+	{
+	  delayState[(PlayerNumber)i].rotate += delayTime;
+	  KeyStates[(PlayerNumber)i].Rotate = true;
+	}
+	if (delayState[(PlayerNumber)i].softDrop < 0)
+	{
+	  delayState[(PlayerNumber)i].softDrop += delayTime;
+	  KeyStates[(PlayerNumber)i].SoftDrop = true;
+	}
+	if (delayState[(PlayerNumber)i].left < 0)
+	{
+	  delayState[(PlayerNumber)i].left += delayTime;
+	  KeyStates[(PlayerNumber)i].Left = true;
+	}
+	if (delayState[(PlayerNumber)i].right < 0)
+	{
+	  delayState[(PlayerNumber)i].right += delayTime;
+	  KeyStates[(PlayerNumber)i].Right = true;
+	}
+	if (delayState[(PlayerNumber)i].hardDrop < 0)
+	{
+	  delayState[(PlayerNumber)i].hardDrop += delayTime;
+	  KeyStates[(PlayerNumber)i].HardDrop = true;
+	}
+	if (delayState[(PlayerNumber)i].hold < 0)
+	{
+	  delayState[(PlayerNumber)i].hold += delayTime;
+	  KeyStates[(PlayerNumber)i].Hold = true;
+	}
+      }
+    }
 
-    protected override void DoLoad() { }
-
-    protected override void DoUpdate(FrameEventArgs e) { }
-
-    protected override void DoDraw(FrameEventArgs e) { }
+    public void End()
+    {
+      for (int i = 1; i < (int)players + 1; i++)
+      {
+	previousState[(PlayerNumber)i].rotate   = currentState[(PlayerNumber)i].rotate;
+	previousState[(PlayerNumber)i].softDrop = currentState[(PlayerNumber)i].softDrop;
+	previousState[(PlayerNumber)i].left     = currentState[(PlayerNumber)i].left;
+	previousState[(PlayerNumber)i].right    = currentState[(PlayerNumber)i].right;
+	previousState[(PlayerNumber)i].hardDrop = currentState[(PlayerNumber)i].hardDrop;
+	previousState[(PlayerNumber)i].hold     = currentState[(PlayerNumber)i].hold;
+      }
+    }
   }
 }
