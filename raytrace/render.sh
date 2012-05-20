eaa4a760e85bb6c64a1e41f91b47f635809d1336
Joseph Dillon <joe@w00t42.net>
2012-05-20 17:45:11 -0400

    more raytraces added

--- raytrace/render.sh
+++ raytrace/render.sh
@@ -6,4 +6,10 @@ povray render[64x64];
 povray render[128x128];
 povray render[256x256];
 povray render[Glade];
-convert ./icon/icon8x8.png ./icon/icon16x16.png ./icon/icon32x32.png ./icon/icon64x64.png ./icon/icon128x128.png ./icon/icon256x256.png ../icon.ico
+povray render[VGA];
+povray render[SVGA];
+povray render[XGA];
+povray render[WXGA];
+povray render[SXGA];
+povray render[WQUXGA];
+convert ./icon/icon8x8.png ./icon/icon16x16.png ./icon/icon32x32.png ./icon/icon64x64.png ./icon/icon128x128.png ./icon/icon256x256.png ../icon.ico;
